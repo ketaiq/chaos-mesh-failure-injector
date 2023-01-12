2f9631c133ef269463dde839c0e5f4a84e3fc241
Ketai Qiu <ketai@usi-macbook-pro.home>
2023-01-12 22:34:57 +0100

    Add linear network delay

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This sends HTTP requests to a chaosd server to inject certain failures.
 
 ## Network Delay
+
+Do not interrupt the program otherwise it won't recover failures!
--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from app.failures.network.network_delay import NetworkDelayFailure
 
 
+def submit_linear_network_delay_failure(num: int, initial_delay: int, inc: int):
+    address = "34.132.63.119:31767"
+    duration = "3m"
+    device = "eth0"
+    workflow = []
+    for i in range(num):
+        latency = initial_delay + i * inc
+        network_delay = NetworkDelayFailure(
+            address,
+            f"{latency}ms-network-delay",
+            duration,
+            device=device,
+            latency=f"{latency}ms",
+        )
+        workflow.append(network_delay)
+
+    for process in workflow:
+        print(f"Submit process {process.name}")
+        process.submit()
+
+
 def main():
-    network_delay_failure = NetworkDelayFailure(
-        "34.132.63.119:31767",
-        "network-delay-failure",
-        "5m",
-        device="eth0",
-        latency="100ms",
-    )
-    network_delay_failure.submit()
+    submit_linear_network_delay_failure(5, 50, 50)
 
 
 if __name__ == "__main__":
